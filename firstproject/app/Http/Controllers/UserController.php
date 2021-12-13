ba69f30455f3348e910775bb06c46864bc91314f
Elen Ardiyanto <elard.yanto@gmail.com>
2021-12-13 10:35:13 +0700

    login controller

---
+++ firstproject/app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Validation\Validator;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        //validation rules
+        $rules = array(
+            'username' => 'required',
+            'password' => 'required|min:8'
+        );
+
+        $validator = User::where('username', '=', $rules['username'])->first();
+        if($validator){
+            if (($request->password == $validator->password))
+            {
+                session(['username' => $request->username]);
+                //return redirect('/adminlte/index');
+            }
+            else {
+                //return back()->withInput()->with('pesan',"Login Gagal");
+            }
+        }
+        else{
+            //return back()->withInput()->with('pesan',"Login Gagal");
+        }
+    }
+
+    public function logout(){
+        session()->forget('username');
+        //return redirect('adminlte/login')->with('pesan','Logout berhasil');
+    }
+}
