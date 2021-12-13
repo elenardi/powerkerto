1fd4ec3b8313433c7771c5c28ff823d7e2d4c546
Elen Ardiyanto <elard.yanto@gmail.com>
2021-12-13 09:51:52 +0700

    first

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,7 +24,6 @@ class UserSeeder extends Seeder
             'username'  => 'admin',
             'email'     => 'admin@admin.com',
             'password'  => Hash::make('12345678'),
-            //haloo
         ));
     }
 }
